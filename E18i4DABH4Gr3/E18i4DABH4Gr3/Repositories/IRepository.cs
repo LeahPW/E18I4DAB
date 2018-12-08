2a31724a70d70cec19ac1be17c58b57163957d89
S. Rasmussen <sigrott@protonmail.com>
2018-12-08 15:59:52 +0100

    Start making repo pattern

---
+++ E18i4DABH4Gr3/E18i4DABH4Gr3/Repositories/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace E18iDABH4Gr3.Repositories
+{
+    public interface IRepository<T>
+    {
+        Task Create(T t);
+        Task<T> Read(int id);
+        Task Update(int id, T t);
+        Task<IEnumerable<T>> ReadAll();
+        Task Delete(T t);
+    }
+}
