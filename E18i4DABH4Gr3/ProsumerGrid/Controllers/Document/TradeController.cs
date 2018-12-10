2eb5625cbb37b08009201ce67a8d1cb14e381a79
grottrup <grottrup@protonmail.com>
2018-12-10 20:14:42 +0100

    Update controller

--- E18i4DABH4Gr3/ProsumerGrid/Controllers/Document/TradeController.cs
+++ E18i4DABH4Gr3/ProsumerGrid/Controllers/Document/TradeController.cs
@@ -1,8 +1,11 @@
-﻿using E18i4DABH4Gr3.Repositories;
+﻿using E18i4DABH4Gr3.Models;
+using E18i4DABH4Gr3.Repositories;
+using Microsoft.Azure.Documents;
 using Microsoft.Azure.Documents.Client;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Http;
 
@@ -12,6 +15,9 @@ namespace ProsumerGrid.Controllers.Document
     {
         private const string _endpointUrl = "https://localhost:8081";
         private const string _key = "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
+        private const string _databaseId = "TradeDB";
+        private const string _collectionId = "TradeCollection";
+
 
         private readonly DocumentClient _client;
         private TradeRepository _repository;
@@ -19,8 +25,13 @@ namespace ProsumerGrid.Controllers.Document
         public TradeController()
         {
             _client = new DocumentClient(new Uri(_endpointUrl), _key);
+            _client.CreateDatabaseIfNotExistsAsync(new Database() { Id = _databaseId });
+            _repository = new TradeRepository(_client, _databaseId, _collectionId);
+        }
 
-            _repository = new TradeRepository(_client, "TradeDB", "TradeCollection");
+        public async Task Post([FromBody]Trade trade)
+        {
+            await _repository.Create(trade);
         }
 
     }
