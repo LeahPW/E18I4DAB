5606fb821fc95b804530464f55ddd16000fa7d48
LeahPW <kolonP@live.dk>
2018-12-11 15:10:55 +0100

    All controllers tested

---
+++ E18i4DABH4Gr3/ProsumerGrid/Migrations/201812111351039_BackToInitial1.cs
@@ -0,0 +1,16 @@
+namespace ProsumerGrid.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class BackToInitial1 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
