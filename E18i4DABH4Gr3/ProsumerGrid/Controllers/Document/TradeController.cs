c6c60fce09ae5d52168be6e1c46f58773b2744b8
grottrup <grottrup@protonmail.com>
2019-01-06 19:51:18 +0100

    Fix cosmos db

--- E18i4DABH4Gr3/ProsumerGrid/Controllers/Document/TradeController.cs
+++ E18i4DABH4Gr3/ProsumerGrid/Controllers/Document/TradeController.cs
@@ -13,19 +13,20 @@ namespace ProsumerGrid.Controllers.Document
 {
     public class TradeController : ApiController
     {
+        // cosmos connection layer
         private const string _endpointUrl = "https://localhost:8081";
-        private const string _key = "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
+        private const string _primaryKey = "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
         private const string _databaseId = "TradeDB";
         private const string _collectionId = "TradeCollection";
-
-
-        private readonly DocumentClient _client;
+        private static DocumentClient _client; // keep alive
         private TradeRepository _repository;
 
         public TradeController()
-        {
-            _client = new DocumentClient(new Uri(_endpointUrl), _key);
+        {          
+            var databaseUri = UriFactory.CreateDatabaseUri(_databaseId);
+            _client = new DocumentClient(new Uri(_endpointUrl), _primaryKey);
             _client.CreateDatabaseIfNotExistsAsync(new Database() { Id = _databaseId });
+            _client.CreateDocumentCollectionIfNotExistsAsync(databaseUri, new DocumentCollection{Id = _collectionId}).Wait();
             _repository = new TradeRepository(_client, _databaseId, _collectionId);
         }
 
@@ -39,5 +40,18 @@ namespace ProsumerGrid.Controllers.Document
             await _repository.Create(trade);
         }
 
+        public async Task<IEnumerable<Trade>> Get()
+        {
+            return await _repository.ReadAll();
+        }
+
+        //// POST: api/Trade
+        public async Task Post([FromBody]List<Trade> trades)
+        {
+            foreach (Trade t in trades)
+            {
+                await _repository.Create(t);
+            }
+        }
     }
 }
